aab60adb767e8938ce7f3a1855782918c2724e6d
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-18 01:46:23 -0500

    Created viewEmployees function

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function init() {
 
 function prompt() {
   inquirer
-    .promopt([
+    .prompt([
       {
         type: "list",
         message: "What would you like to do?",
@@ -73,7 +73,22 @@ function prompt() {
     });
 }
 
-function viewEmployees() {}
+function viewEmployees() {
+  dbConnection.query(
+    `SELECT employee.id, employee.first_name AS firstname, employee.last_name AS lastname, role.title AS position, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id`,
+    function (err, res) {
+      if (err) throw err;
+      console.log("\n");
+      console.table(res);
+      prompt();
+    }
+  );
+}
 
 function addEmployee() {}
 
