278ce9976b27159cf3c74acf3560b340e3958ca1
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-17 23:20:09 -0500

    Connected data base to mysql

--- server.js
+++ server.js
@@ -3,3 +3,19 @@ const express = require("express");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 require("console.table");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    database: "employee_db",
+    host: "localhost",
+    user: "root",
+    password: "password2015",
+  },
+  console.log(`Connected to the employee_db database.`)
+);
